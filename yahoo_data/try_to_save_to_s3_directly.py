c37fd7c483764be5bcdca3cc9c2946313205b81f
Vladimir Ryzhikov <vladimirryzhikov@icloud.com>
2023-03-20 02:11:57 +0300

    Added description and readme

--- yahoo_data/try_to_save_to_s3_directly.py
+++ yahoo_data/try_to_save_to_s3_directly.py
@@ -6,16 +6,8 @@ import pandas as pd
 
 
 AWS_S3_BUCKET = "bronzelayer"
-AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
-AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
-AWS_SESSION_TOKEN = os.getenv("AWS_SESSION_TOKEN")
 
-s3_client = boto3.client(
-    "s3",
-    aws_access_key_id=AWS_ACCESS_KEY_ID,
-    aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
-    aws_session_token=AWS_SESSION_TOKEN,
-)
+s3_client = boto3.client("s3")
 
 tickers_df = pd.read_csv("data/all_tickers.csv")
 
